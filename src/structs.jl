2a7edee1dca8556094d4fb1f6b14bf11a30c4e84
Taylor McDonnell <taylor.golden.mcdonnell@gmail.com>
2018-02-22 10:14:01 -0700

    change parametric type to be more flexible

--- src/structs.jl
+++ src/structs.jl
@@ -1,14 +1,14 @@
-struct material{T<:Real}
-  e1::T #E1
-  e2::T #E2
-  g12::T #G12
-  nu12::T #nu12
-  rho::T #density
-  xt::T #longitudinal tensile ultimate strength (Pa)
-  xc::T #longitudinal compressive ultimate strength (Pa)
-  yt::T #transverse tensile ultimate strength (Pa)
-  yc::T #transverse compressive ultimate strength (Pa)
-  s::T #shear ultimate strength (Pa)
+struct material
+  e1::T where T<:Real #E1
+  e2::T where T<:Real #E2
+  g12::T where T<:Real #G12
+  nu12::T where T<:Real #nu12
+  rho::T where T<:Real #density
+  xt::T where T<:Real #longitudinal tensile ultimate strength (Pa)
+  xc::T where T<:Real #longitudinal compressive ultimate strength (Pa)
+  yt::T where T<:Real #transverse tensile ultimate strength (Pa)
+  yc::T where T<:Real #transverse compressive ultimate strength (Pa)
+  s::T where T<:Real #shear ultimate strength (Pa)
 end
 
 function material(e1::Array{Real,1},e2::Array{Real,1},g12::Array{Real,1},
@@ -28,8 +28,8 @@ end
 struct laminate
   matid::Array{Int64,1}
   nply::Array{Int64,1}
-  tply::Array{Real,1}
-  theta::Array{Real,1}
+  tply::Array{T,1} where T<:Real
+  theta::Array{T,1} where T<:Real
   function laminate(matid::Array{Int64,1},nply::Array{Int64,1},tply::Array{Real,1},theta::Array{Real,1})
     if !(length(matid) == length(nply) == length(tply) == length(theta))
       error("All arrays must have same length")
